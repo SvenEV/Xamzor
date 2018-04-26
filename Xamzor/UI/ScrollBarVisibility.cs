feef01e236b82acdd5edafed1f1514b1b7703897
Sven Vinkemeier <svenvinkemeier@live.de>
2018-04-26 17:43:27 +0200

    Added limited support for ScrollViewer.ZoomMode and ScrollViewer.ScrollBarVisibility

---
+++ Xamzor/UI/ScrollBarVisibility.cs
@@ -0,0 +1,17 @@
+﻿namespace Xamzor.UI
+{
+    public enum ScrollBarVisibility
+    {
+        /// <summary>
+        /// Only works in Edge.
+        /// </summary>
+        Auto,
+
+        /// <summary>
+        /// Only works in Edge, Webkit.
+        /// </summary>
+        Hidden,
+
+        Visible,
+    }
+}
---
+++ Xamzor/UI/ZoomMode.cs
@@ -0,0 +1,14 @@
+﻿namespace Xamzor.UI
+{
+    public enum ZoomMode
+    {
+        Disabled,
+
+        /// <summary>
+        /// Scroll mode must be <see cref="ScrollMode.Enabled"/> or <see cref="ScrollMode.Auto"/>
+        /// in both directions for zooming to be enabled.
+        /// Only works in Edge.
+        /// </summary>
+        Enabled
+    }
+}
